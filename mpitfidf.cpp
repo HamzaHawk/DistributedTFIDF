12f5a653e14b15270a9a7cce9e40d173c4dc49be
Kim Paterson <kimberly@paterson.org>
2013-12-06 19:22:33 -0800

    made updates to the main to add pseudo code

--- mpitfidf.cpp
+++ mpitfidf.cpp
@@ -3,11 +3,13 @@
  */
 
 #include "mpitfidf.hpp"
-#define MASTER 0
 
 using namespace MPI;
 
-int main(int argc, char *argv[]) {
+
+int main(int argc, char *argv[]) 
+{
+   
    if (argc < 2) {
       printf("Usage: mpitfidf <file1> [.... <fileN>]\n");
       return 1;
@@ -19,8 +21,8 @@ int main(int argc, char *argv[]) {
    
    //create array of files
    char *files[argc-1];
-
-   int i;   
+   int i;
+      
    for (i = 0; i < argc-1; i++) {
       files[i] = argv[i+1];
    }
@@ -28,7 +30,7 @@ int main(int argc, char *argv[]) {
    //parse arguments
    Init(argc,argv);
    me = COMM_WORLD.Get_rank();
-   numProcs = COMM_WORLD.Get_size();
+   numProcs = COMM_WORLD.Get_size();   
 
    if (me == MASTER) {
       printf("Initialized...\n");
@@ -39,8 +41,38 @@ int main(int argc, char *argv[]) {
       printf("Divided files...\n");
    }
 
+   //get doc_count from each file
+   
+   //combine doc counts from each node
 
-   Finalize();   
+   //get tf_max from each node
+
+
+   //combine tf_max into global max vectors
+
+
+   //IN CHILD NODE
+   
+   //calc tf for each doc
+
+   //calc max for each word
+
+   //send doc count to master
+
+   //send max to doc count
+
+   //for d in D
+   //for w in d
+   //calc normalized tfidf
+
+   //output
+   
+
+
+
+
+
+   Finalize();
    return 0;
+   
 }
-
--- mpitfidf.hpp
+++ mpitfidf.hpp
@@ -1,8 +1,7 @@
 /*
  *
  */
-
-#include "mpi.h"
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
