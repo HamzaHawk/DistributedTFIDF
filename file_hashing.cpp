fc89ba4cefae7e8bf63a1120275edcfe8dec151b
Andrew Sinclair <asinclair4@gmail.com>
2013-12-06 16:52:53 -0800

    Added function to create the node wide document count for each word.

--- file_hashing.cpp
+++ file_hashing.cpp
@@ -4,7 +4,6 @@
 #include <map>
 #include <vector>
 
-
 struct letter_only: std::ctype<char> 
 {
     letter_only(): std::ctype<char>(get_table()) {}
@@ -30,9 +29,42 @@ std::map<std::string, int> countWordsInFile(std::string filename) {
      {
          ++wordCount[word];
      }
+     /* Print code for debugging purposes.
      for (std::map<std::string, int>::iterator it = wordCount.begin(); it != wordCount.end(); ++it)
      {
            cout << it->first <<" : "<< it->second << endl;
      }
+     */
      return wordCount;
 }
+
+std::map<string, int> countDocumentsContainingWords(std::vector< std::map<std::string, int> > wordCounts) {
+   using namespace std;
+   map<string, int> documentCount;
+   for(vector< map<string, int> >::iterator it = wordCounts.begin(); it != wordCounts.end(); ++it) {
+      for(map<string,int>::iterator iter = (*it).begin(); iter != (*it).end(); ++iter) {
+         ++documentCount[iter->first];
+      }
+   }
+
+   /* Print code for debugging purposes.
+   for(map<string, int>::iterator it = documentCount.begin();it != documentCount.end(); ++it) {
+      cout << it->first << " : " << it->second << endl;
+   }
+   */
+   return 
+}
+
+int main() {
+   using namespace std;
+   map<string, int> wordCount = countWordsInFile("file_hashing.cpp");
+   map<string, int> otherWordCount = countWordsInFile("file_hashing.hpp");
+   vector< map<string,int> > perFileWordCounts;
+   perFileWordCounts.push_back(wordCount);
+   perFileWordCounts.push_back(otherWordCount);
+   countDocumentsContainingWords(perFileWordCounts);
+}
+
+
+
+
