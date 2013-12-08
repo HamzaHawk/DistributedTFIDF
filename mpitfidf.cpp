5cbb4d976fb9296493f6ab2cf94dfff05910a73d
Kim Paterson <kimberly@paterson.org>
2013-12-08 13:28:19 -0800

    removed max logic in main

--- mpitfidf.cpp
+++ mpitfidf.cpp
@@ -53,25 +53,13 @@ int main(int argc, char *argv[])
       }
 
       //call count documents with vector
-      set<string> listOfWords;
-      map <string, int> docCounts = countDocumentsContainingWords(fileCounts, listOfWords);
-            
-      //calculate max for each document 
-      map <string, int > *maxCounts = max_term_finder(fileCounts, listOfWords);
-       
-      //serialize
-      int max_size;
-      int doc_size;
 
-      //serialize data
-      char *max_buf = map_serialize(*maxCounts, &max_size);
+      map <string, int> docCounts = countDocumentsContainingWords(fileCounts);
+            
+      //serialize
+      int doc_size;
       char *doc_buf = map_serialize(docCounts, &doc_size);
 
-      //send to max master
-      COMM_WORLD.Send(&max_size, 1, MPI_INT, MASTER, me);
-      COMM_WORLD.Send(max_buf, max_size, MPI_BYTE,
-                      MASTER, MAX_TF_TYPE);
-      
       //send doc counts ot master
       COMM_WORLD.Send(&doc_size, 1, MPI_INT, MASTER, me);
       COMM_WORLD.Send(doc_buf, doc_size, MPI_BYTE,
@@ -86,37 +74,20 @@ int main(int argc, char *argv[])
    char *global_doc_buf;
    
    if (me == MASTER) {
-      map <string, map <string, int> > max_buffer_map;
       map <string, map <string, int> > doc_buffer_map;
-
-      int max_buf_size;
       int doc_buf_size;
       
       int rank;
       
       for (i = 0; i < numProcs-1; i++) {
-         //receive size from child -- tag is rank of node
-         COMM_WORLD.Recv(&max_buf_size, 1, MPI_INT, MPI_ANY_SOURCE, rank);
-         int type;
-         
-         char *max_buffer = (char *)malloc(max_buf_size);         
-         COMM_WORLD.Recv(max_buffer, max_buf_size, MPI_BYTE, rank, type);
-
-         //deserialize
-         map <string, int> *max_map = map_deserialize(max_buffer);
-         
-//         char *temp = itoa(rank);
-         //       string str_temp(temp);
-         std::stringstream ss;
-         ss << rank;
-         std::string temp = ss.str();
-
-         //add to map
-         max_buffer_map[temp] = *max_map;
-         
          //get document counts from each node
          COMM_WORLD.Recv(&doc_buf_size, 1, MPI_INT, MPI_ANY_SOURCE, rank);
          int doc_type;
+
+         //make rank a string
+         std::stringstream ss;
+         ss << rank;
+         std::string temp = ss.str();
          
          char *doc_buffer = (char *)malloc(doc_buf_size);         
          COMM_WORLD.Recv(doc_buffer, doc_buf_size, MPI_BYTE, rank, doc_type);
@@ -128,36 +99,19 @@ int main(int argc, char *argv[])
          doc_buffer_map[temp] = *doc_map;
       }
       
-      //user doc & max vector
-      //TODO listofwords is undefined
-      set<string> listOfWords;
-      map <string, int> *global_max_map = max_term_finder(max_buffer_map, listOfWords);
-      
-      //serialize
-      global_max_buf = map_serialize(*global_max_map, &global_max_size);
-      
-      //document_freq
+      //create global doc map
       map <string, int> *global_doc_map = reduceDocumentFrequencies(doc_buffer_map);
 
       global_doc_buf = map_serialize(*global_doc_map, &global_doc_size);
       
    } 
-
-   //broadcast
-   //broadcast global_max_size
-   COMM_WORLD.Bcast(&global_max_size, 1, MPI_INT, me);
-
    //broadcase global_doc_size
    COMM_WORLD.Bcast(&global_doc_size, 1, MPI_INT, me);
    
    //malloc space for buffers
    if (me != MASTER) {
-      global_max_buf = (char *)malloc(global_max_size);
       global_doc_buf = (char *)malloc(global_doc_size);
    }
-
-   //broadcast(global_max_buf);   
-   COMM_WORLD.Bcast(&global_max_buf, global_max_size, MPI_BYTE, me);
 
    //broadcast(global_doc_buf);
    COMM_WORLD.Bcast(&global_doc_buf, global_doc_size, MPI_BYTE, me);
@@ -167,9 +121,6 @@ int main(int argc, char *argv[])
    }
 
    if (me != MASTER) {
-      //deserialized max_buf
-      map <string, int> *global_max_map = map_deserialize(global_max_buf);
-      
       //deserialize doc_buf
       map <string, int> *global_doc_map = map_deserialize(global_doc_buf);
 
@@ -183,7 +134,6 @@ int main(int argc, char *argv[])
    if (me == MASTER) {
       printf("Done.");
    }
-   
 
    Finalize();
    return 0;
