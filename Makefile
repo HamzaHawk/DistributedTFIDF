80a33823da476458f1cd4f59e5d9943f27113a5e
Kim Paterson <kimberly@paterson.org>
2013-12-07 19:04:45 -0800

    totally broken at this time

--- Makefile
+++ Makefile
@@ -5,8 +5,8 @@ LIBS=-lmpi
 
 all: mpitfidf.o
 
-mpitfidf.o: mpitfidf.cpp file_hashing.cpp max_term_finder.cpp
-	$(CC) $(OBJ_FLAGS) mpitfidf.cpp file_hashing.cpp max_term_finder.cpp
+mpitfidf.o: mpitfidf.cpp file_hashing.cpp max_term_finder.cpp document_frequency.cpp serialize_utility.cpp
+	$(CC) $(OBJ_FLAGS) mpitfidf.cpp file_hashing.cpp max_term_finder.cpp document_frequency.cpp serialize_utility.cpp
 
 clean:
 	rm -rf mpitfidf *.o
--- mpitfidf.cpp
+++ mpitfidf.cpp
@@ -5,6 +5,8 @@
 #include "mpitfidf.hpp"
 #include "file_hashing.hpp"
 #include "max_term_finder.hpp"
+#include "serialize_utility.hpp"
+#include "document_frequency.hpp"
 
 using namespace MPI;
 
@@ -41,15 +43,15 @@ int main(int argc, char *argv[])
       printf("Divided files...\n");
    }
 
+   map <string, map <string, int> > fileCounts;
+
    if (me != MASTER) {
-      vector <map <string, int> > fileCounts;
-      
       for (i = 0; i < numFiles; i++) {
          // if index % numPRocs-1 +1 is equal to rank, process doc (omits master)
          if ((i%(numProcs-1) + 1) == me) {
             //read document calculate tf for each document i
             map<string, int> wordCount = countWordsInFile(files[i]);
-            fileCounts.push_back(wordCount);
+            fileCounts[files[i]]=wordCount;
          }
       }
 
@@ -69,49 +71,26 @@ int main(int argc, char *argv[])
       char *doc_buf = map_serialize(&docCounts, &doc_size);
 
       //send to max master
-      COMM_WORLD.send(&max_size, 1, MPI_INT, MASTER, me);
-      COMM_WORLD.send(max_buf, &max_size, MPI_BYTE,
+      COMM_WORLD.Send(&max_size, 1, MPI_INT, MASTER, me);
+      COMM_WORLD.Send(max_buf, &max_size, MPI_BYTE,
                       MASTER, MAX_TF_TYPE);
       
       //send doc counts ot master
-      COMM_WORLD.send(&doc_size, 1, MPI_INT, MASTER, me);
-      COMM_WORLD.send(doc_buf, &doc_size, MPI_BYTE,
+      COMM_WORLD.Send(&doc_size, 1, MPI_INT, MASTER, me);
+      COMM_WORLD.Send(doc_buf, &doc_size, MPI_BYTE,
                    MASTER, DOC_F_TYPE);
 
-      //TODO receive global doc and max
-      int global_max_size;
-      
-      COMM_WORLD.recv(&global_max_size, 1, MPI_INT, MASTER, 0);
-      char *global_max_buf = (char *)malloc(global_max_size);
-      
-      //get the global max
-      COMM_WORLD.recv(global_max_buf, global_max_size, MPI_BYTE, MASTER, 0);
-      
-      //deserialize global max
-      map<string, int> *global_max_map = map_deserialize(global_max_buf);
-
-      int global_doc_size;
-      
-      COMM_WORLD.recv(&global_doc_size, 1, MPI_INT, MASTER, 0);
-      char *global_doc_buf = (char *)malloc(global_doc_size);
-      
-      //get the global max
-      COMM_WORLD.recv(global_doc_buf, global_doc_size, MPI_BYTE, MASTER, 0);
-      
-      //deserialize global max
-      map<string, int> *global_doc_map = map_deserialize(global_doc_buf);
-      
-      //TODO calculations
-      
-      //calc TFIDF
-     
-      //output
-
    }
+
+   int global_max_size;   
+   char *global_max_buf;
+
+   int global_doc_size;   
+   char *global_doc_buf;
    
    if (me == MASTER) {
-      vector <map <string, int> > max_buffer;
-      vector <map <string, int> > doc_buffer;
+      map <string, map <string, int> > max_buffer_map;
+      map <string, map <string, int> > doc_buffer_map;
 
       int max_buf_size;
       int doc_buf_size;
@@ -120,42 +99,86 @@ int main(int argc, char *argv[])
       
       for (i = 0; i < numProcs-1; i++) {
          //receive size from child -- tag is rank of node
-         COMM_WORLD.recv(&max_buf_size, 1, MPI_INT, MPI_ANY_SOURCE, rank);
+         COMM_WORLD.Recv(&max_buf_size, 1, MPI_INT, MPI_ANY_SOURCE, rank);
          int type;
          
          char *max_buffer = (char *)malloc(buf_size);         
-         COMM_WORLD.recv(max_buffer, max_buf_size, MPI_BYTE, rank, type);
+         COMM_WORLD.Recv(max_buffer, max_buf_size, MPI_BYTE, rank, type);
 
          //deserialize
          map <string, int> max_map = map_deserialize(max_buffer);
          
-         //add to vector
-         max_buffer.push_back(max_map);
+         //add to map
+         max_buffer_map[std::to_string(rank)] = max_map;
          
          //get document counts from each node
-         COMM_WORLD.recv(&doc_buf_size, 1, MPI_INT, MPI_ANY_SOURCE, rank);
+         COMM_WORLD.Recv(&doc_buf_size, 1, MPI_INT, MPI_ANY_SOURCE, rank);
          int doc_type;
          
          char *doc_buffer = (char *)malloc(doc_buf_size);         
-         COMM_WORLD.recv(doc_buffer, doc_buf_size, MPI_BYTE, rank, doc_type);
+         COMM_WORLD.Recv(doc_buffer, doc_buf_size, MPI_BYTE, rank, doc_type);
          
          //deserialize
          map <string, int> doc_map = map_deserialize(doc_buffer);
 
          //add to vector
-         doc_buffer.push_back(doc_map);
-         
+         doc_buffer_map[std::to_string(rank)] = doc_map;
       }
       
       //user doc & max vector
-      //TODO max_term_finder
-
+      map <string, int> *global_max_map = max_term_finder(max_buffer_map);
+      
+      //serialize
+      global_max_buf = map_serialize(global_max_map, &global_max_size);
+      
       //document_freq
+      map <string, int> *global_doc_map = reduceDocumentFrequencies(doc_buffer_map);
 
-      //broadcast size to all nodes?
-      //serialize & broadcast to all nodes
+      global_doc_buf = map_serialized(global_doc_map, &global_doc_size);
       
    } 
+
+   //broadcast
+   //broadcast global_max_size
+   COMM_WORLD.Bcast(&global_max_size, 1, MPI_INT, me);
+
+   //broadcase global_doc_size
+   COMM_WORLD.Bcast(&global_doc_size, 1, MPI_INT, me);
+   
+   //malloc space for buffers
+   if (me != MASTER) {
+      global_max_buf = (char *)malloc(global_max_size);
+      global_doc_buf = (char *)malloc(global_doc_size);
+   }
+
+   //broadcast(global_max_buf);   
+   COMM_WORLD.Bcast(&global_max_buf, global_max_size, MPI_BYTE, me);
+
+   //broadcast(global_doc_buf);
+   COMM_WORLD.Bcast(&global_doc_buf, global_doc_size, MPI_BYTE, me);
+
+   if (me == MASTER) {
+      printf("Calculating and outputting...");
+   }
+
+   if (me != MASTER) {
+      //deserialized max_buf
+      map <string, int> *global_max_map = map_deserialize(global_max_buf);
+      
+      //deserialize doc_buf
+      map <string, int> *global_doc_map = map_derserialize(global_doc_buf);
+
+      //TODO calculations
+      
+      //calc TFIDF
+     
+      //output
+   }
+   
+   if (me == MASTER) {
+      printf("Done.");
+   }
+   
 
    Finalize();
    return 0;
